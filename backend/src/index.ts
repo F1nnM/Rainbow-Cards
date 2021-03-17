6c26b1c24ad8460d40bc42b5c4edd01794d49e97
F1nnM <github@mfinn.de>
2021-03-17 22:13:07 +0100

    Save work before test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import { Server } from "colyseus";
 import { monitor } from "@colyseus/monitor";
+import { GameRoom } from "./rooms/GameRoom/GameRoom";
 
 const port = Number(process.env.PORT || 2567);
 const app = express()
@@ -14,6 +15,8 @@ const server = http.createServer(app);
 const gameServer = new Server({
   server,
 });
+
+gameServer.define('game', GameRoom)
 
 // register colyseus monitor AFTER registering your room handlers
 app.use("/colyseus", monitor());
--- backend/src/rooms/GameRoom/GameRoom.ts
+++ backend/src/rooms/GameRoom/GameRoom.ts
@@ -1,5 +1,6 @@
 import { Dispatcher } from "@colyseus/command";
-import { Room, Client } from "colyseus";
+import { Room, Client, Delayed } from "colyseus";
+import { CloseCommand } from "./commands/CloseCommand";
 import { CzarVoteCommand } from "./commands/CzarVoteCommand";
 import { NewRoundCommand } from "./commands/NewRoundCommand";
 import { PlayCardCommand } from "./commands/PlayCardCommand";
@@ -10,10 +11,14 @@ export class GameRoom extends Room<GameRoomState> {
 
   dispatcher = new Dispatcher(this);
 
+  noCreatorReturnClose: Delayed;
+
   onCreate (options: any) {
+    this.autoDispose = false;
     this.setState(new GameRoomState());
 
-    this.setPrivate(options.private)
+    this.state.private = options.private;
+    this.setPrivate(true) // private until creator rejoined
 
     this.clock.start()
 
@@ -36,24 +41,60 @@ export class GameRoom extends Room<GameRoomState> {
   }
 
   onJoin (client: Client, options: any) {
+    if(!this.state.creatorLeft)
+      return;
+    if(!this.state.owner){
+      // creator joined again
+      this.state.owner = client.sessionId;
+      this.autoDispose = true;
+      this.setPrivate(this.state.private)
+      this.noCreatorReturnClose.clear()
+    }
     this.state.players.set(client.sessionId, new Player());
   }
 
   async onLeave (client: Client, consented: boolean) {
-    this.state.players.get(client.sessionId).connected = false;
+    if (this.state.creatorLeft){
+      console.log(client.sessionId+" left")
+      this.state.players.get(client.sessionId).connected = false;
 
-    try {
-      if (consented) {
-          throw new Error("consented leave");
+      // if the owner left replace them after 10 seconds.
+      let replaceOwnerTimeout;
+      if (this.state.owner == client.sessionId){
+        console.log(client.sessionId + " was owner, searching new")
+        replaceOwnerTimeout = this.clock.setTimeout(() => {
+          let newOwner;
+          this.state.players.forEach((player) => {
+            if(player.connected)
+              newOwner = player.id
+          })
+          if (newOwner){
+            console.log("found new owner: "+newOwner)
+            this.state.owner = newOwner
+          }
+          else{
+            console.log("no new owner found, disconnecting all")
+            this.disconnect()
+          }
+        }, 10_000);
       }
-
-      await this.allowReconnection(client);
-
-      this.state.players.get(client.sessionId).connected = true;
-
-    } catch (e) {
-      this.state.players.delete(client.sessionId);
+        
+      // if (consented) {
+      //   this.state.players.get(client.sessionId).left = true;
+      // }else{
+        try{
+          await this.allowReconnection(client, 60_000*5);
+          if(replaceOwnerTimeout)
+            replaceOwnerTimeout.clear() // owner returned, don't replace
+          this.state.players.get(client.sessionId).connected = true;
+        } catch {}
+      //}
+    }else{
+      console.log("Creator left")
+      this.state.creatorLeft = true;
+      this.noCreatorReturnClose = this.clock.setTimeout(()=>this.disconnect, 60_000) //kill room if the creator doesn't return in a minute.
     }
+
   }
 
   onDispose() {
